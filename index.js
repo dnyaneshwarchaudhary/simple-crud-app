9412df13abd89facb3bdf6e9d131848ef40dbe6c
Dnyaneshwar Chaudhary <dnyaneshwar.bhartendu1972@gmail.com>
2024-03-13 23:19:56 +0530

    simplecrudapp

--- index.js
+++ index.js
@@ -15,56 +15,6 @@ app.get('/',(req,res) => {
 });
 
 
-
-app.get('/api/products/:id',async (req,res)=> {
-
-    try{
-      const {id} = req.params;
-      const product = await Product.findById(id)
-      res.status(200).json(product);
-  }
-  catch(error){
-    res.status(500).json({message: error.message});
-  }
-})
-
-app.put('/api/products/:id',async (req,res)=> {
-
-  try{
-    const {id} = req.params;
-    const product = await Product.findByIdAndUpdate(id,req.body)
-    res.status(200).json(product);
-}
-catch(error){
-  res.status(500).json({message: error.message});
-}
-})
-
-app.post('/api/products',async (req,res) => {
-    try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    }
-    catch(error){
-      res.status(500).json({message: error.message});
-    }
-});
-
-app.delete('/api/products/:id', async (req,res) => {
-  try{
-
-    const {id} = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product){
-      return res.status(404).json({message:"Product not found"})
-    }
-    res.status(200).json({message: "Product Deleted Successfully"});
-}
-catch(error){
-  res.status(500).json({message: error.message});
-}
-})
-
 mongoose.connect('mongodb+srv://dnyaneshwarworkin:Danny%402001@backenddb.ce9jmgf.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() => {
     console.log('Connected! to database');
